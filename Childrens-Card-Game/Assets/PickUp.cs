1e9fa85079cf98e741b9792e059959e3c48b7884
oernymn <47537746+oernymn@users.noreply.github.com>
2019-07-02 14:25:51 +0200

    We can pick up card now! Pog
    Just browsed the internet for solutions :D XD

---
+++ Childrens-Card-Game/Assets/PickUp.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class PickUp : MonoBehaviour
+{
+
+
+    bool clicked = false;
+
+    float height = 2.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (clicked == true)
+        {
+            Vector3 temp = Input.mousePosition;
+            temp.z = 75f; // Set this to be the distance you want the object to be placed in front of the camera.
+            transform.position = Camera.main.ScreenToWorldPoint(temp);
+        }
+        Debug.Log(transform.position);
+    }
+
+
+    void OnMouseDown()
+    {
+        if (clicked == false)
+        {
+            clicked = true;
+        } else
+        {
+            clicked = false;
+        }
+    }
+
+
+}
